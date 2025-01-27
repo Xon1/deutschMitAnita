e24aa4ee62c6d389898f375b8ece7e114e1ed726
Xon1 <ep52768@ubt-uni.net>
2025-01-28 00:05:41 +0100

    Add Dark mode off

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui']
+  modules: ['@nuxt/ui'],
+  colorMode: {
+    preference: 'light', // Gjithmonë prefero "light"
+    fallback: 'light',   // Nëse preferenca mungon, përdor "light"
+    classSuffix: '',     // Pa shtesa në klasat CSS
+    storage: undefined,      // Mos ruaj preferencat
+    // detectSystemColorScheme: false,
+    storageKey: 'nuxt-color-mode',
+  },
 })
